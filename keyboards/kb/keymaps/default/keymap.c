d87af23f04da2a0aa8af74ad460e4318338fa735
corlos <corlos@users.noreply.github.com>
2021-02-08 22:59:42 -0600

    Update keymap.c

--- keyboards/kb/keymaps/default/keymap.c
+++ keyboards/kb/keymaps/default/keymap.c
@@ -30,6 +30,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
+
+
+
+
+void matrix_init_user(void) {
+}
+
+void matrix_scan_user(void) {
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	switch (keycode) {
 		 case ZERK:
@@ -77,18 +87,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	return true;
 }
 
-
-
-
-void matrix_init_user(void) {
-}
-
-void matrix_scan_user(void) {
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	return true;
-}
 
 void led_set_user(uint8_t usb_led) {
 
